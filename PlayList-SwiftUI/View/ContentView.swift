841791883729b0e91d26436d79143197ad66ce9d
Zohra18 <ach.zohra@gmail.com>
2020-03-01 21:41:13 +0100

    Doing test for now, will update later

--- PlayList-SwiftUI/View/ContentView.swift
+++ PlayList-SwiftUI/View/ContentView.swift
@@ -9,32 +9,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sliderValue: Double = 25
+    
     var body: some View {
         
-        NavigationView {
-            
-            List(userList) { users in
-                NavigationLink(destination: UserProfile(user: users)) {
-                    HStack(alignment: .center, spacing: 5) {
-                        Image("")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .background(Color.purple)
-                            .cornerRadius(10)
-                        Text("\(users.username)")
-
+                NavigationView {
+        
+                    List(userList, id: \.self) { users in
+                        NavigationLink(destination: UserProfile(user: users)) {
+                            HStack(alignment: .center, spacing: 5) {
+                                Image("")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .background(Color.purple)
+                                    .cornerRadius(10)
+                                VStack {
+                                    Text("\(users.username)")
+//                                    Text("\(users.id)")
+                                }
+                                
+                                
+        
+                            }
+                        }
                     }
-                }
-            }
-        .navigationBarTitle("Trainers")
-        } // fin de NavView
+                .navigationBarTitle("Trainers")
+                } // fin de NavView
+//        ZStack {
+//
+//
+//            if sliderValue <= 15 {
+//                Color(.cyan)
+//            }
+//            else if sliderValue <= 35 {
+//                Color(.orange)
+//            }
+//            else {
+//                Color(.red)
+//            }
+//
+//
+//            VStack {
+//                Spacer()
+//                Text("\(Int(sliderValue)) °c")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//
+//                if sliderValue <= 15 {
+//                    Text("It's way to cold")
+//                }
+//                    else if sliderValue <= 35 {
+//                    Text("Temp is just right cap'")
+//                }
+//                else {
+//                    Text("Woah... I'm melting")
+//                }
+//
+//                Slider(value: $sliderValue, in: 0...50)
+//                Spacer()
+//
+//            }
+//            .padding(30)
+//
+//
+//        }
+//        .edgesIgnoringSafeArea(.all)
         
         
         
         
-        
-        } // fin de body
-    } // find de ContentView
+    } // fin de body
+} // find de ContentView
 
 
 struct ContentView_Previews: PreviewProvider {
